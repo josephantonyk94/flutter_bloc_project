b10ac7df3af76907296f8780b484811c74fb7e5b
josephantonyk94 <josephantonyk94@gmail.com>
2020-11-05 22:33:28 +0530

    first commiy

---
+++ android/app/src/main/kotlin/com/akj94/flutter_blocs_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akj94.flutter_blocs_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+void main()=>runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Login();
+  }
+}
